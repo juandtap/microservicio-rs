d172c405d10676c1f92dd787254f815556af79dd
Diego Tapia <jtapiav4@est.ups.edu.ec>
2024-01-02 23:44:59 -0500

    Initial commit

---
+++ src/main/java/ups/config/AppConfig.java
@@ -0,0 +1,10 @@
+package ups.config;
+
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class AppConfig extends Application {
+
+}
---
+++ src/main/java/ups/model/Curso.java
@@ -0,0 +1,6 @@
+package ups.model;
+
+public class Curso {
+
+    
+}

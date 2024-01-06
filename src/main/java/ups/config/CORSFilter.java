1f31f84e21484f5f839fd6a04c9a86356e30ce25
Diego Tapia <jtapiav4@est.ups.edu.ec>
2024-01-06 14:39:38 -0500

    Update CORSFilter.java

--- src/main/java/ups/config/CORSFilter.java
+++ src/main/java/ups/config/CORSFilter.java
@@ -4,7 +4,7 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
-
+import jakarta.ws.rs.core.Response;
 import java.io.IOException;
 
 @Provider
@@ -12,8 +12,10 @@ public class CORSFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         responseContext.getHeaders().add("Access-Control-Max-Age", "-1");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     }
 }
